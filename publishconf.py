907aa52890ea0102792ec48eb3940adf4de925f7
Senran Zhang <zsrkmyn@gmail.com>
2024-03-23 19:04:13 +0800

    Add post crypto-coin-arbitrage

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ATOM = 'feeds/atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
